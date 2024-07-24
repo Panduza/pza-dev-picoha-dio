b1b88cd0cc3271164e30da62f6c7124be34140a6
XdoctorwhoZ <xavier.rodriguezalvarez@gmail.com>
2024-07-24 16:56:56 +0100

    start test impl

--- tests/robot/libraries/usb_keywords.py
+++ tests/robot/libraries/usb_keywords.py
@@ -1,8 +1,33 @@
+import serial
+import logging
+import serial.tools.list_ports
+
+from robot.libraries.BuiltIn import BuiltIn
 
 
-
+###############################################################################
 
 def I_list_all_the_USB_devices():
-    pass
+    devices_object = []
+    devices = serial.tools.list_ports.comports()
+    for device in devices:
+        devices_object.append({
+            "port_name": device.device,
+            "pid": device.pid,
+            "vid": device.vid,
+            "serial_number": device.serial_number
+        })
+    logging.debug("All devices: ", devices_object)
+    BuiltIn().set_global_variable('${ALL_DEVICES}', devices_object)
 
+###############################################################################
+
+def find_port_name_from_USB_ids(vid, pid):
+    devices = serial.tools.list_ports.comports()
+    for device in devices:
+        if device.vid == vid and device.pid == pid:
+            return device.device
+    return None
+
+###############################################################################
 
--- tests/robot/picoha-dio/2-robustness.robot
+++ tests/robot/picoha-dio/2-robustness.robot
@@ -9,12 +9,18 @@ Test Teardown       Main Test Platform Cleanup
 *** Test Cases ***
 
 Test corrupted data
-    Given a serial connection to the device
+    Given A serial connection to the device is opened
     When I send "100" random bytes to the device
-    Then the device must still be able to respond correctly
+    Then The device must still be able to respond correctly
 
 *** Keywords ***
+
+# A serial connection to the device is opened
+    
 
 I send "${number_of_bytes}" random bytes to the device
     Log    message: ${number_of_bytes}
     Log    need to implement
+
+
+
