dd12dbff99fc3b9fae65d028ae329fd7bd99b58b
Jason Puel <jason.puel@gmail.com>
2024-09-20 10:27:14 +0200

    Update link with Keyworlds

--- test/robot/libraries/gpio_keywords.py
+++ test/robot/libraries/gpio_keywords.py
@@ -23,53 +23,44 @@ def is_connected():
         return "false"
     
 def ping():
+    '''send Ping frame'''
     try :
         return "true" if (test.ping_info() == dio.AnswerType.SUCCESS) else "false"
     except:
         return "false"
 
-def set_gpio_mode(gpio:int, mode:dio.PinValue):
-    '''set gpio mode AND complementary gpio mode according to shematics'''
-    comp_gpio = get_comp_gpio(gpio)
-    logging.debug(f'{gpio} is connected to {comp_gpio}')
-    if mode == dio.PinValue.INPUT:
-        err1 = test.set_gpio_mode(gpio, dio.PinValue.INPUT)
-        err2 = test.set_gpio_mode(comp_gpio, dio.PinValue.OUTPUT)
-    elif mode == dio.PinValue.OUTPUT:
-        err1 = test.set_gpio_mode(gpio, dio.PinValue.OUTPUT)
-        err2 = test.set_gpio_mode(comp_gpio, dio.PinValue.INPUT)
+def set_gpio_direction(gpio:int, direction:dio.PinValue):
+    '''set gpio direction'''
+    if direction == 'INPUT':
+        err = test.set_gpio_direction(gpio, dio.PinValue.INPUT)
+    elif direction == 'OUTPUT':
+        err = test.set_gpio_direction(gpio, dio.PinValue.OUTPUT)
     else:
-        logging.debug("This is not a direction value")
-        err1,err2 = dio.AnswerType.FAILURE
-    if err1 == dio.AnswerType.SUCCESS and err2 == dio.AnswerType.SUCCESS:
-        logging.debug("SUCCESS")
-        return ("SUCCESS")
-    else :
-        logging.debug("FAILURE")
-        return ("FAILURE")
+        logging.warning(f"This is not Direction value : {direction}")
+        err = dio.AnswerType.FAILURE
+    
+    return "SUCCESS" if err == dio.AnswerType.SUCCESS else "FAILURE"
 
-def get_gpio_mode(gpio:int) -> str :
-    '''return the gpio mode'''
-    direction = test.get_gpio_mode(gpio)
+def get_gpio_direction(gpio:int):
+    '''return the gpio direction'''
+    direction = test.get_gpio_direction(gpio)
     if direction == dio.PinValue.INPUT:
         return 'INPUT'
     elif direction == dio.PinValue.OUTPUT:
         return 'OUTPUT'
     else :
-        logging.warning("this is not Direction value")
+        logging.warning(f"This is not Direction value : {direction}")
         return None
 
 def set_gpio_value(gpio:int, value):
-    '''set the gpio mode'''
+    '''set the gpio direction'''
     if test.set_gpio_value(gpio, value) == dio.AnswerType.SUCCESS:
-        logging.debug("SUCCESS")
         return ("SUCCESS")
     else :
-        logging.debug("FAILURE")
         return ("FAILURE")
 
 def get_gpio_value(gpio:int):
-    '''return the gpio mode'''
+    '''return the gpio direction'''
     value = test.get_gpio_value(gpio)
     if value == dio.PinValue.LOW:
         return 'LOW'
@@ -91,16 +82,30 @@ def get_comp_gpio(gpio:int):
     elif gpio == 25 :
         logging.warning('GPIO 25 is builtin LED')
         return "FAILURE"
-    elif gpio==22:
-        return 26
-    elif gpio==26:
-        return 22
-    elif gpio==27: 
-        return 28
-    elif gpio==28: 
-        return 27
+    elif gpio==22: return 26
+    elif gpio==26: return 22
+    elif gpio==27: return 28
+    elif gpio==28: return 27
     else :
         return gpio-1 if gpio % 2 else gpio+1
+     
+def set_gpio_direction_and_his_comp(gpio:int, direction:dio.PinValue):
+    '''set gpio direction AND complementary gpio direction according to shematics'''
+    comp_gpio = get_comp_gpio(gpio)
+    logging.debug(f'{gpio} is connected to {comp_gpio}')
+    if direction == 'INPUT':
+        err1 = test.set_gpio_direction(gpio, dio.PinValue.INPUT)
+        err2 = test.set_gpio_direction(comp_gpio, dio.PinValue.OUTPUT)
+    elif direction == 'OUTPUT':
+        err1 = test.set_gpio_direction(gpio, dio.PinValue.OUTPUT)
+        err2 = test.set_gpio_direction(comp_gpio, dio.PinValue.INPUT)
+    else:
+        logging.warning(f"This is not Direction value : {direction}")
+        err1,err2 = dio.AnswerType.FAILURE
+    if err1 == dio.AnswerType.SUCCESS and err2 == dio.AnswerType.SUCCESS:
+        return ("SUCCESS")
+    else :
+        return ("FAILURE")
     
 def get_gpio_value_comp(gpio:int):
     return get_gpio_value(get_comp_gpio(gpio))
