76bb86017cc283271a12119ac9b3b243da50f6d6
Gregory Soutade <gregory.soutade@elsys-design.com>
2025-02-21 15:54:28 +0100

    Add benchmark test

---
+++ test/cucumber/tests/benchmark.rs
@@ -0,0 +1,14 @@
+mod libs;
+use libs::connectors::UsbSettings;
+
+use cucumber::World;
+
+use libs::world::PiochaWorld;
+
+#[tokio::main]
+async fn main() {
+    PiochaWorld::cucumber()
+        .init_tracing()
+        .run("features/benchmark.feature")
+        .await;
+}
