fcdc29f24ced4bfa83c09dad366b50e0e49c8b22
Jason Puel <jason.puel@gmail.com>
2025-03-19 11:25:12 +0100

    Add docker file

--- test/robot/report-serve.sh
+++ test/robot/report-serve.sh
@@ -1,9 +1,53 @@
 #!/bin/bash
+ACTION=$1
+IMG='robot-report-server'
 
-# Start web server
-docker run --rm -d --name robot-report-server \
-    -v $PWD:/usr/share/nginx/html:ro \
-    -v $PWD/nginx.conf:/etc/nginx/conf.d/default.conf:ro \
-    -p 8080:80 nginx
+cd $(dirname "$0")
 
-# docker container stop robot-report-server
+case $ACTION in
+    build)
+        echo -e " -------------------------------------------\n"\
+                "Generation docker image: ${IMG}\n"\
+                "-------------------------------------------"
+        docker build . -t ${IMG}
+        ;;
+    status)
+        echo -e " ---------------------------------\n"\
+                "Checking the status of ${IMG}\n"\
+                "---------------------------------"
+        DATA=$(docker ps | grep ${IMG})
+        [[ -z $DATA ]] && echo "No Process" || echo "$DATA"
+        ;;
+
+    stop)
+        echo -e " -----------------------------------------\n"\
+                "Stopping docker instance ${IMG}\n"\
+                "-----------------------------------------"
+        docker stop ${IMG}
+        ;;
+
+    start)
+        echo -e " --------------------------------------------------\n"\
+                "Starting and connecting the instance ${IMG}\n"\
+                "--------------------------------------------------"
+        docker run -it --rm -d --name ${IMG} -p 8080:80 nginx
+        ;;
+
+    run)
+        echo -e " --------------------------------------------------\n"\
+                "Run instance ${IMG}.\n"\
+                "--------------------------------------------------"
+        docker run -it --entrypoint sh ${IMG}
+        ;;
+    *)
+        echo "$0 [stop/start/status/build]"
+        echo -e "\t* stop   : stops the Docker instance"
+        echo -e "\t* start  : start the Docker instance and/or connect you to bash"
+        echo -e "\t* status : displays the status of your instance"
+        echo -e "\t* build  : rebuilds the Docker image ${IMG}"
+        echo -e "\t* run    : run the Docker instance"
+        exit 0
+        ;;
+esac
+
+cd -
